9f03f4360782a7bdc7b96dbf0030d13864f73658
Maarten de Vries <maarten@de-vri.es>
2020-10-17 15:17:26 +0200

    Add tests for Modulo utility trait.

--- src/util.rs
+++ src/util.rs
@@ -7,3 +7,22 @@ impl Modulo for i32 {
 		((self % b) + b) % b
 	}
 }
+
+#[cfg(test)]
+mod test {
+	use super::*;
+	use assert2::assert;
+
+	#[test]
+	fn modulo() {
+		assert!((8).modulo(12) == 8);
+		assert!((20).modulo(12) == 8);
+		assert!((-4).modulo(12) == 8);
+		assert!((-16).modulo(12) == 8);
+
+		assert!((12).modulo(12) == 0);
+		assert!((24).modulo(12) == 0);
+		assert!((-12).modulo(12) == 0);
+		assert!((-24).modulo(12) == 0);
+	}
+}
